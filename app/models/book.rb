fc0f8bcb555534e55881c0935a62e2ce65545a40
Rodrigue NGORAN <rodrigue.ngoran@kosmopolead.com>
2024-05-15 09:38:06 +0200

    Validation test

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,4 @@
 class Book < ApplicationRecord
+    validates :title, :author, presence: true
+    validates :page_count, numericality: {only_integer: true, greater_than: 0}, allow_blank: true
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -13,6 +13,11 @@ require 'rails_helper'
   # code refactoring
 RSpec.describe Book, type: :model do
   let(:book) { FactoryBot.build(:book)}  # Permet d'affecter une variable à une expression à évaluer
+  
+  #it { should validate_presence_of(:title) }  # Validation de presence avec shoulda matchers
+  #it { should validate_uniqueness_of(:author) } # Validation de presence avec shoulda matchers
+  
+  
   it "can be instanciated" do
     expect(book).not_to be nil
   end
@@ -20,4 +25,29 @@ RSpec.describe Book, type: :model do
   it "can be saved" do
     expect(book.save).to be true
   end
+
+  it "is invalid without title" do
+    book.title = nil 
+    expect(book.valid?).to be false
+    expect(book.errors.messages).to have_key(:title)
+  end
+
+  it "is invalid without author" do
+    book.author= nil 
+    expect(book.valid?).to be false
+    expect(book.errors.messages).to have_key(:author)
+  end
+
+  it "is invalid with a negative page count" do
+    book.page_count = -1
+    expect(book.valid?).to be false
+    expect(book.errors.messages).to have_key(:page_count)
+  end
+
+  it "is invalid with a 0 page count" do
+    book.page_count = 0
+    expect(book.valid?).to be false
+    expect(book.errors.messages).to have_key(:page_count)
+  end
+
 end
